3ca3d43a351cce0d5a61c9bdd83e703be91e0924
Kishor Bhagat <kishorebhagat663@gmail.com>
2023-07-02 02:12:23 +0530

    added Modal form

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -7,17 +7,31 @@ const GlobalStyle = createGlobalStyle`
         --text-primary: #fff;
         --text-secondary: #9a9aa8;
         --text-tertiary: #d2d2e0;
-        --hover: #34343f;
+        --hover: #36374b;
+        --hover-light: rgba(54, 55, 75, 0.3);
+        --pink: #e756b5;
+        --avatar: #9a9aa8;
+        --avatar-font-color: #272836;
     }
     .light-theme{
         --background-primary: #dfdfe3;
-        /* --background-primary: #d0d0d6; */
+        /* --background-primary: #d6d6db; */
+        /* --background-primary: #b4b5bd; */
+        /* --background-primary: #cecfd3; */
+
         --background-secondary: #bfc0c6;
         /* --background-secondary: #b4b5bd; */
+        /* --background-secondary: #d6d6db; */
+        /* --background-secondary: #e4e4ef; */
+
         --text-primary: #000;
         --text-secondary: #000;
         --text-tertiary: #000;
         --hover: #a9aab1;
+        --hover-light: rgba(169, 170, 177, 0.5);
+        --pink: #e756b5;
+        --avatar: #e756b5;
+        --avatar-font-color: #fff;
     }
 
     *{
